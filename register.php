98a6b846adee58999616026eac480a3fb86f847b
snow-sagene <146735906+snow-sagene@users.noreply.github.com>
2023-10-04 22:03:50 +0900

    Add files via upload

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Flekke</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+</head>
+<body>
+    hotdogs
+</body>
+</html>
